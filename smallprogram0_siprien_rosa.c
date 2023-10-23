ccb2f69f6830601cf483fb855b74e435723fed68
Rosa <104663000+rosasip@users.noreply.github.com>
2023-10-23 09:41:44 -0400

    main

---
+++ smallprogram0_siprien_rosa.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("The current month is August.\n");
+    printf("The current year is 2023. \n");
+    printf("I am a student at the University of Central Florida. \n");
+    printf("This Fall 2023, I am taking COP3223 Introduction to Programming with C with Dr. Steinberg. \n");
+    printf("I am super excited to learn the C Language this semester! \n");
+    printf("Marvel Studios will release The Marvels in November. Yay! \n");
+    
+    return 0;
+}
